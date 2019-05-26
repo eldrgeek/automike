dbf2405af3ad1031a65d84f94d3e4e98ef6d6b99
Mike Wolf <mw@mike-wolf.com>
2019-05-25 23:34:56 -0400

    textarea change callback test

--- src/components/Dictation.js
+++ src/components/Dictation.js
@@ -2,13 +2,22 @@ import React from 'react'
 import "../css/dictation.css"
 // import init from "../DictationController.js"
 let init = () => {}
+
+let onChange = (event) => {
+    let statusArea = document.querySelector('#status')
+    console.log(event.target,event);
+    if(event.target && event.target.value) {
+    console.log("value", event.target.value)
+    statusArea.textContent = event.target.value;
+    }
+}
 const Dictation = function(props) {
     let initStore = () => {init(props.store)}
     return <div className="container">
     Stuff at the top
         <div className="text-box" id="text" >
 
-            <textarea className="textarea" id="textarea" />
+            <textarea className="textarea" id="textarea" onChange={onChange} />
 
         </div>
         <div id="footer">
