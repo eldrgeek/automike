42378609d8032ccc9647c3ff2dd54c6435f4cedc
Mike Wolf <mw@mike-wolf.com>
2019-06-17 10:40:55 -0400

    Added React Speech Recognition to demo

--- src/components/Dictation.js
+++ src/components/Dictation.js
@@ -1,31 +1,74 @@
-import React from 'react'
-import "../css/dictation.css"
+import React from "react";
+import "../css/dictation.css";
 // import init from "../DictationController.js"
-let init = () => {}
+import SpeechRecognition from "react-speech-recognition";
+let init = () => {};
 
-let onChange = (event) => {
-    let statusArea = document.querySelector('#status')
-    console.log(event.target,event);
-    if(event.target && event.target.value) {
-    console.log("value", event.target.value)
+let onChange = event => {
+  let statusArea = document.querySelector("#status");
+  console.log(event.target);
+
+  if (event.target && event.target.value) {
+    console.log("value", event.target.value);
     statusArea.textContent = event.target.value;
+  }
+};
+
+const options = {
+  autoStart: true,
+  continuous: true
+};
+
+let listening = options.autoStart;
+
+const Dictation = function({
+  store,
+  transcript,
+  resetTranscript,
+  startListening,
+  stopListening,
+  browserSupportsSpeechRecognition
+}) {
+  let initStore = () => {
+    init(store);
+  };
+  const toggleListening = () => {
+    if (listening) {
+      stopListening();
+    } else {
+      startListening();
     }
-}
-const Dictation = function(props) {
-    let initStore = () => {init(props.store)}
-    return <div className="container">
-    Stuff at the top
-        <div className="text-box" id="text" >
-
-            <textarea className="textarea" id="textarea" onChange={onChange} />
-
-        </div>
-        <div id="footer">
-            <button id="start" onClick={initStore}>Starting</button>
-            <button id="stop">Stop</button>
-            <i className="fa fa-microphone"></i>
-            <p className="status" id="status" contentEditable="true" />
-        </div>
+    listening = !listening;
+  };
+  const onTextClick = event => {
+    console.log("click", event.target.selectionStart);
+  };
+  //console.log(transcript)
+  return (
+    <div className="container">
+      Stuff at the top if (transcript) console.log(transcript)
+      <div className="text-box" id="text">
+        <textarea
+          className="textarea"
+          id="textarea"
+          onClick={onTextClick}
+          onChange={onChange}
+          value={"this" + transcript + "that"}
+        />
+      </div>
+      <div id="footer">
+        <button id="start" onClick={stopListening}>
+          Stop
+        </button>
+        <button onClick={resetTranscript} id="stop">
+          Reset
+        </button>
+        <i onClick={toggleListening} className="fa fa-microphone" />
+        <p className="status" id="status" contentEditable="true" />
+      </div>
     </div>
-}
-export default Dictation;
+  );
+};
+
+export default SpeechRecognition(options)(Dictation);
+//export default Dictation;
